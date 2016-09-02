50e9ec5d22ec000815e14f8a24f4be8379ec0612
BoltzmannBrain <alexdlavin@gmail.com>
2016-09-02 13:48:17 -0700

    optical flow generator, and cleanup

--- models/optical_flow.py
+++ models/optical_flow.py
@@ -1,29 +1,22 @@
 """
+Tools for optical flow tracking with OpenCV.
 """
-
-
-
 import cv2
 import numpy as np
 
 
 
 class OpticalFlow(object):
+  """ Module for optical flow tracking with feature point detection.
   """
-  """
-  def __init__(self, videoSource, featurePtMask=None, verbosity=0):
-    # self.videoStream = cv2.VideoCapture(videoSource)
-    # self.tracks = []
-    # self.frameIdx = 0
 
+  def __init__(self, videoSource, featurePtMask=None, verbosity=0):
     # cap the length of optical flow tracks
     self.maxTrackLength = 10
 
     # detect feature points in intervals of frames; adds robustness for
     # when feature points disappear.
     self.detectionInterval = 5
-
-    # self.prevGray = None
 
     # Params for Shi-Tomasi corner (feature point) detection
     self.featureParams = dict(
@@ -85,7 +78,7 @@ class OpticalFlow(object):
     self.videoStream.set(1, relativePosition)
 
 
-  def run(self):
+  def runAll(self):
     """
     Run through the video stream. Must initialize the camera first.
 
@@ -143,7 +136,7 @@ class OpticalFlow(object):
           if not _verifyPtWithinDomain(np.uint32(track[-1])):
             continue
           # Calculate the delta btwn this pt and the last pt in the track
-          if len(track) > 0:
+          # if len(track) > 0:
             # # Get this track's delta -- pixel distance from previous frame
             # deltas.append(np.linalg.norm(newPt-track[-1]))
           # Add this pt to the optical flow track
@@ -160,7 +153,7 @@ class OpticalFlow(object):
         # frameSeconds = self.videoStream.get(0)/1000.0
         # streamOfDeltas.append([frameSeconds, deltas])
 
-        # Draw multiple lines:
+        # Draw multiple lines
         cv2.polylines(frameVis, [np.int32(track) for track in tracks], False, (0, 255, 0))
 
       # Detect feature pts every few frames
@@ -190,15 +183,111 @@ class OpticalFlow(object):
         # End of video file, or user quit with 'ESC'
         break
 
-    self.cam.release()
+    self.videoStream.release()
     cv2.destroyAllWindows()
 
     # TODO: refactoring to be a generator would be better!
     # return streamOfDeltas
 
 
-  def _verifyPtWithinDomain(self, trackPt):
+  def runGenerator(self):
+    """ Iterate through the video stream, yielding an optical flow mask for each
+    frame.
+
+    For each frame, we get the points of the optical flow tracks, calculate
+    their spatial changes from the last frame ("deltas"), draw their tracking
+    lines, and re-detect feature points (every few frames).
+
+
     """
+    tracks = []
+    frameIndex = 0
+    while self.videoStream.isOpened():
+      # Read in this frame, and get the grey mask of it
+      ret, frame = self.videoStream.read()
+      if ret is False:
+        continue
+      frameGrey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+      # Use a frame copy for drawing
+      frameVis = frame.copy()
+
+      if len(tracks) > 0:
+        # Points are the (x,y) pixel coords of the optical flow tracks. Use
+        # the most recent track points (old) to calculate the (new) points
+        # for this frame.
+        pointsOld = np.float32([tr[-1] for tr in tracks]).reshape(-1, 1, 2)
+        pointsNew, statuses, _ = cv2.calcOpticalFlowPyrLK(
+            prevGreyFrame, frameGrey, pointsOld, None, **self.lkParams)
+
+        # Use back-tracking to verify feature point matches between frames, and
+        # the status to ignore "lost" points.
+        pointsOldReversed, _, _ = cv2.calcOpticalFlowPyrLK(
+            frameGrey, prevGreyFrame, pointsNew, None, **self.lkParams)
+        maxDiffs = abs(pointsOld-pointsOldReversed).reshape(-1, 2).max(-1)
+        goodPoints = np.logical_and(maxDiffs < 1, statuses.reshape(maxDiffs.shape[0]))
+
+        # Update optical flow tracks, track their changes (deltas)
+        # deltas = []
+        updatedTracks = []
+        for track, newPt, good in zip(tracks, pointsNew.reshape(-1, 2), goodPoints):
+          if not good:
+            # Bad track, ignore it
+            continue
+          if not _verifyPtWithinDomain(np.uint32(track[-1])):
+            continue
+          # Add this pt to the optical flow track
+          x, y = newPt[0], newPt[1]
+          track.append((x, y))
+          if len(track) > self.maxTrackLength:
+            # Only maintain the recent portion of the tracks (for drawing)
+            del track[0]
+          # Draw a circle for this optical flow pt.
+          cv2.circle(frameVis, (x, y), 2, (0, 255, 0), -1)
+          updatedTracks.append(track)
+
+        tracks = updatedTracks
+
+        # Draw multiple lines
+        cv2.polylines(frameVis, [np.int32(track) for track in tracks], False, (0, 255, 0))
+
+      # Detect feature pts every few frames
+      if frameIndex % self.detectionInterval == 0:
+        # Draw a circle for each feature pt
+        for x, y in [np.int32(tr[-1]) for tr in tracks]:
+          cv2.circle(self.featurePtMask, (x, y), 5, 0, -1)
+        # Detect corners with Shi-Tomasi algorithm
+        featurePts = cv2.goodFeaturesToTrack(
+            frameGrey, mask=self.featurePtMask, **self.featureParams)
+        # featurePts = self.fast.detect(frameGrey, None)
+        if featurePts is not None:
+          # Add feature points to the optical flow tracks
+
+          for featureCoordinates in featurePts:  # TODO: do this better with itertools?
+            x = featureCoordinates[0][0]  # ShiTomasi
+            y = featureCoordinates[0][1]  # ShiTomasi
+            # x = featureCoordinates.pt[0]  # fast features
+            # y = featureCoordinates.pt[1]  # fast features
+            tracks.append([(x, y)])
+
+      frameIndex += 1
+      prevGreyFrame = frameGrey
+      cv2.imshow("tracked_frame", frameVis)
+
+      if (self.videoSource.get(2) == 1) or (cv2.waitKey(1) & 0xFF==27):
+        # End of video file, or user quit with 'ESC'
+        break
+
+      # TODO: better to yield just the most recent pt of each track
+      yield tracks
+
+    self.videoStream.release()
+    cv2.destroyAllWindows()
+
+
+  def _verifyPtWithinDomain(self, trackPt):
+    """ Returns False if the track point is (projected) out of the video frame
+    or the domain mask.
     """
     if (trackPt[0]>self.height-1 or trackPt[0]<0) or
        (trackPt[1]>self.width-1 or trackPt[1]<0):
